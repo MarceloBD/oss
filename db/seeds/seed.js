fd3408fc58a09386de139c3b6fdb948475dad095
marcelobd <marcelo.diani@usp.br>
2019-09-25 02:01:57 -0300

    Add seed

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,7 +1,12 @@
-exports.seed = function(knex, Promise) {
-  return knex("user")
-    .del()
-    .then(() => {
-      return knex("user").insert([{ name: "marcelo" }]);
-    });
+exports.seed = async (knex, Promise) => {
+  await knex("user").del();
+
+  const userIds = await knex("user").insert([
+    {
+      name: "Marcelo Diani",
+      email: "marcelo.diani@usp.br",
+      document: "41528788869",
+      password: "123"
+    }
+  ]);
 };
